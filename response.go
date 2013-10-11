a2ebe348e534e78b94d51ff64cedf07bb4fc93c3
Kentaro Kuribayashi <kentarok@gmail.com>
2013-10-11 13:28:34 +0900

    Fix a bug that server crushes when connection refused.

--- response.go
+++ response.go
@@ -13,17 +13,20 @@ type Response struct {
 	Elapsed      time.Duration
 }
 
-func NewResponse(backend *Backend, httpResponse *http.Response, elapsed time.Duration) (*Response, error) {
-	response := &Response{
+func NewResponse(backend *Backend, httpResponse *http.Response, elapsed time.Duration) (response *Response, err error) {
+	response = &Response{
 		Backend:      backend,
 		HttpResponse: httpResponse,
 		Data:         make([]byte, 0),
 		Elapsed:      elapsed,
 	}
-	data, err := ioutil.ReadAll(httpResponse.Body)
 
-	response.HttpResponse.Body.Close()
-	response.Data = data
+	if httpResponse != nil {
+		var data []byte
+		data, err = ioutil.ReadAll(httpResponse.Body)
+		response.HttpResponse.Body.Close()
+		response.Data = data
+	}
 
 	return response, err
 }
